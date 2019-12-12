6cf0626e9ab78e127e08f4e128df96c5d9b00f51
Belal Khan <belalkhan.dev@gmail.com>
2019-12-12 09:22:30 +0600

    css and js file add

---
+++ public/assets/js/custom.js
@@ -0,0 +1,31 @@
+(function ($) {
+    "use-strict"
+
+    jQuery(document).ready(function () {
+        
+        // //navigation active
+        // $(document).on('click', '.has-dropdown a', function () {
+        //     $(this).closest('.has-dropdown').toggleClass('active');
+        // });
+
+
+        // //active class
+        // var current = location.pathname;
+
+        // if (current) {
+        //     current = current.split('/').pop();
+        // }
+
+        // $('.navigation ul li a').each(function(){
+        //     var $this = $(this);
+        //     // if the current path is like this link, make it active
+        //     if($this.attr('href').indexOf(current) !== -1){
+        //         $this.closest('.has-dropdown').addClass('active');
+        //         $this.closest('li').addClass('active');
+        //     }
+        // })
+
+    });
+
+
+}(jQuery));
