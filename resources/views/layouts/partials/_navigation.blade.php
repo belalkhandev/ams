fcf33e64de4412e7af19943a4bf8d980e067db62
Belal Khan <belalkhan.dev@gmail.com>
2019-12-18 09:29:20 +0600

    navigation menu add

--- resources/views/layouts/partials/_navigation.blade.php
+++ resources/views/layouts/partials/_navigation.blade.php
@@ -1,26 +1,66 @@
 <nav class="navigation-menu">
     <ul>
         <li><a href="index.html"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
-        <li class="nav-title">Lorem ipsum</li>
         <li class="treeview">
-            <a href="javascript:void(0)"><i class="fas fa-list"></i><span>Table</span><i class="fas fa-angle-right"></i></a>
+            <a href="javascript:void(0)">
+                <i class="fas fa-list"></i>
+                <span>Admission Management</span>
+                <i class="fas fa-angle-right"></i></a>
             <ul class="treeview-menu">
-                <li><a href="#">Basic Table</a></li>
-                <li><a href="#">Datatable</a></li>
+                <li><a href="#">New Admission</a></li>
+                <li><a href="#">Admission List</a></li>
             </ul>
         </li>
         <li class="treeview">
-            <a href="javascript:void(0)"><i class="fas fa-leaf"></i><span>Form</span><i class="fas fa-angle-right"></i></a>
+            <a href="javascript:void(0)">
+                <i class="fas fa-list"></i>
+                <span>Attendance</span>
+                <i class="fas fa-angle-right"></i></a>
             <ul class="treeview-menu">
-                <li><a href="#">Basic Form</a></li>
-                <li><a href="#">Advanced Form</a></li>
+                <li><a href="#">Upload Attendance</a></li>
+                <li><a href="#">Attendance Processing</a></li>
+            </ul>
+        </li>
+        <li class="nav-title">Class manage</li>
+        <li class="treeview">
+            <a href="javascript:void(0)">
+                <i class="fas fa-list"></i>
+                <span>Class</span>
+                <i class="fas fa-angle-right"></i></a>
+            <ul class="treeview-menu">
+                <li><a href="#">New Class</a></li>
+                <li><a href="#">Class list</a></li>
             </ul>
         </li>
         <li class="treeview">
-            <a href="javascript:void(0)"><i class="fas fa-lock"></i><span>Authentication</span><i class="fas fa-angle-right"></i></a>
+            <a href="javascript:void(0)">
+                <i class="fas fa-list"></i>
+                <span>Group</span>
+                <i class="fas fa-angle-right"></i></a>
             <ul class="treeview-menu">
-                <li><a href="pages/login.html">Login</a></li>
-                <li><a href="#">Registration</a></li>
+                <li><a href="#">New Group</a></li>
+                <li><a href="#">Group List</a></li>
+            </ul>
+        </li>
+        <li class="treeview">
+            <a href="javascript:void(0)">
+                <i class="fas fa-list"></i>
+                <span>Section</span>
+                <i class="fas fa-angle-right"></i></a>
+            <ul class="treeview-menu">
+                <li><a href="#">New Section</a></li>
+                <li><a href="#">Section List</a></li>
+            </ul>
+        </li>
+        <li class="nav-title">Routine manage</li>
+        <li class="treeview">
+            <a href="javascript:void(0)">
+                <i class="fas fa-list"></i>
+                <span>Class Routine</span>
+                <i class="fas fa-angle-right"></i></a>
+            <ul class="treeview-menu">
+                <li><a href="#">New Class Routine</a></li>
+                <li><a href="#">Class Routine list</a></li>
             </ul>
         </li>
     </ul>
